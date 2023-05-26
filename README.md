a7a9127512ba7fa618bf7aaf3a7437e5e12cde12
Sebastião Vitor <sebastiaovitor18@gmail.com>
2023-05-25 23:11:02 -0300

    readme

---
+++ README.md
@@ -0,0 +1,38 @@
+# DNC School
+
+## Escola DNC Ciclo Iniciante
+## Recriando Layout
+
+![preview](./img/preview.png)
+
+> Formação em Tecnologia
+
+Projeto Tabuada.
+
+> [🔗 Clique aqui para acessar da Page](https://tabuada-dnc-schooll.netlify.app)
+
+# 🧰🛠️ Tecnologias
+
+- HTML
+- CSS
+- Git e Github
+- Variavéis
+- Acessibilidade
+- WEB
+- FIGMA
+- Fontes e personalizção
+- Function
+- JavaScript
+
+
+# 💛 Contato
+
+sebastiaovitor18@gmail.com
+
+[Linkedin](https://www.linkedin.com/in/sebastião-vitor-7a2870106/)
+=======
+
+Nessa etapa aprendemos as funcionalidades do DOM, recriando uma tabuada.
+
+Ansioso para as próximas etapas.
+😁bora codar.
--- tabuada-01.js
+++ tabuada-01.js
@@ -1,18 +1,21 @@
+let num = window.document.getElementById('num');
+let end = window.document.getElementById('end');
+let res = window.document.getElementById('resposta');
+
+let x = 0;
+let r = '';
+
 function Tabuada() {
-  let num = window.document.getElementById('num');
-  let end = document.getElementById('end')
+  let n = Number(num.value);
+  let e = Number(end.value);
   if (num.value.length == 0 || end.value.length == 0) {
     window.alert('[ERRO...] Confira os dados!!');
   } else {
-    let n = Number(num.value);
-    let e = Number(end.value);
-    let x = 0;
-    let r = '';
-    let res = document.getElementById('resposta');
     res.innerHTML = `A tabuada do ${n} é: </br>`;
     do {
       r = n*e;
       res.innerHTML += `${x} X ${n} = ${r} </br>`;
-    } while(x <= e);
+      x++;
+    } while(x <= e)
   }
 }
