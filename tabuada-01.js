3f174cd79e4c3fb67552744ffc6240ea0b0033cd
Sebastião Vitor <sebastiaovitor18@gmail.com>
2023-05-25 23:23:27 -0300

    sd

--- tabuada-01.js
+++ tabuada-01.js
@@ -2,12 +2,12 @@ let num = window.document.getElementById('num');
 let end = window.document.getElementById('end');
 let res = window.document.getElementById('resposta');
 
-let x = 0;
-let r = '';
 
 function Tabuada() {
   let n = Number(num.value);
   let e = Number(end.value);
+  let x = 0;
+  let r = '';
   if (num.value.length == 0 || end.value.length == 0) {
     window.alert('[ERRO...] Confira os dados!!');
   } else {
